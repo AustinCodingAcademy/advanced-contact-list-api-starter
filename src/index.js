4a0442e7a7a24cec6aaf9787886113e73c0ef468
mhow8809 <mhow_22000@yahoo.com>
2017-02-13 20:54:00 -0600

    completed express starter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 // Your server code here...
+// Your server code here...
+import express from 'express';
+
+// Create a new instance of the express module
+const app = express();
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// Declare our GET /contacts route
+app.get('/contacts', (request, response) => {
+  const contacts = [
+    {
+      _id: 1,
+      name: 'Dale Cooper',
+      occupation: 'FBI Agent',
+      avatar: 'https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg'
+    },
+    {
+      _id: 2,
+      name: 'Spike Spiegel',
+      occupation: 'Bounty Hunter',
+      avatar: 'http://vignette4.wikia.nocookie.net/deadliestfiction/images/d/de/Spike_Spiegel_by_aleztron.jpg/revision/latest?cb=20130920231337'
+    }
+  ];
+
+  return response.json(contacts);
+});
+
+
+// Set our port to server the application on
+const PORT = 3001;
+
+app.listen(PORT, (err) => {
+  if (err) {
+    return console.log('Error!', err);
+  }
+
+  return console.log('Listening on: http://localhost:' + PORT);
+});
