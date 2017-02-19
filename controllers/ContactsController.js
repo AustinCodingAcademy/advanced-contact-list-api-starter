230e211c57eda27670c8b643e2e56855b080442f
dsolis421 <snapshot8d@gmail.com>
2017-02-19 17:50:46 -0600

    working v3, refactored for ES6

--- controllers/ContactsController.js
+++ controllers/ContactsController.js
@@ -2,27 +2,19 @@ import ContactModel from '../src/models/ContactModel';
 
 const contactsController = {};
 
-contactsController.list = function(request, response, next) {
+contactsController.list = (request, response, next) => {
   ContactModel.find().exec()
-  .then(contacts => {
-    return response.json(contacts);
-  })
-  .catch(err => {
-    return next(err);
-  });
+  .then(contacts => response.json(contacts))
+  .catch(err => next(err));
 };
 
-contactsController.show = function(request, response, next) {
+contactsController.show = (request, response, next) => {
   ContactModel.findById(request.params._id).exec()
-  .then(contact => {
-    return response.json(contact);
-  })
-  .catch(err => {
-    return next(err);
-  });
+  .then(contact => response.json(contact))
+  .catch(err => next(err));
 };
 
-contactsController.create = function(request, response, next) {
+contactsController.create = (request, response, next) => {
   // New instance of ContactModel, creating a new record for contacts collection
   // Grabbing attributes and establishing the payload
   const CONTACT = new ContactModel({
@@ -34,16 +26,12 @@ contactsController.create = function(request, response, next) {
   // save the new contact
   CONTACT.save()
   // When save completes, return new contact
-  .then(newContact => {
-    return response.json(newContact);
-  })
-  .catch(err => {
-    return next(err);
-  });
+  .then(newContact => response.json(newContact))
+  .catch(err => next(err));
 };
 
-contactsController.update = function(request, response, next) {
-  ContactModel.findById(request.params._id)
+contactsController.update = (request, response, next) => {
+  ContactModel.findById(request.params._id).exec()
   .then(contact => {
     // Attributes will be from request.body OR contact
     contact.name = request.body.name || contact.name;
@@ -52,22 +40,14 @@ contactsController.update = function(request, response, next) {
 
     return contact.save();
   })
-  .then(contact => {
-    return response.json(contact);
-  })
-  .catch(err => {
-    return next(err);
-  });
+  .then(contact => response.json(contact))
+  .catch(err => next(err));
 };
 
-contactsController.remove = function(request, response, next) {
+contactsController.remove = (request, response, next) => {
   ContactModel.findByIdAndRemove(request.params._id).exec()
-  .then(contact => {
-    return response.json(contact);
-  })
-  .catch(err => {
-    return next(err);
-  });
+  .then(contact => response.json(contact))
+  .catch(err => next(err));
 };
 
 export default contactsController;
--- src/index.js
+++ src/index.js
@@ -6,18 +6,16 @@ import mongoose from 'mongoose';
 // Create a new instance of express
 const app = express();
 
+const contactRoutes = require('../routes/contactRoutes');
+
 // Connect to Mongo database
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/contact-list');
 
+// Middleware
 app.use(bodyParser.json());
-
-const contactRoutes = require('../routes/contactRoutes');
 app.use(contactRoutes);
-
-app.use(function (err, request, response) {
-  return response.status(500).send('Request error! ' + err);
-});
+app.use((err, request, response) => response.status(500).send('Request error! ' + err));
 
 // Set our port to server the application on
 const PORT = 3001;
