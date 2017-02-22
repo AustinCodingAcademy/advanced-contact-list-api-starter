45d01312f184dc245ba29bf67ddc4c71521e8879
Kayne Khoury <kaynekhoury1@gmail.com>
2017-02-21 20:40:37 -0600

    models working

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import ContactModel from './models/ContactModel';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 // initialize express
 const app = express();
@@ -14,7 +15,7 @@ app.use(function (req, res, next) {
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/contact-list');
-
+app.use(bodyParser.json());
 
 // reassign our route
 app.get('/contacts', function (request, response) {
@@ -35,6 +36,81 @@ app.get('contacts/:_id', function (request, response) {
   .catch(err => {
     return console.loge(`Error ${err}`);
   });
+});
+
+app.delete('/contacts/:_id', function (request, response) {
+  ContactModel.findByIdAndRemove(request.params._id).exec()
+    .then(contact => {
+      return response.json(contact);
+    })
+    .catch(err => {
+      return console.log(`Error ${err}`);
+    });
+});
+
+app.post('/contacts', function (request, response) {
+  const newContact = new ContactModel({
+    name: request.body.name,
+    occupation: request.body.occupation,
+    avatar: request.body.avatar,
+  });
+
+  // Save the new contact
+  newContact.save()
+    // When the save completes, return the newly created contact
+    .then(contact => {
+      return response.json(contact);
+    })
+    .catch(err => {
+      return console.log(`Error ${err}`);
+    });
+});
+
+// reassign our route
+app.get('/addedContacts', function (request, response) {
+  ContactModel.find({}).exec()
+    .then(contacts => {
+      return response.json(contacts);
+    })
+    .catch(err => {
+      return console.log(`Error ${err}`);
+    });
+});
+
+app.get('/addedContacts/:_id', function (request, response) {
+  ContactModel.findById(request.params._id).exec()
+  .then(contact => {
+    return response.json(contact);
+  })
+  .catch(err => {
+    return console.log(`Error ${err}`);
+  });
+});
+
+app.post('/addedContacts', function (request, response) {
+  const newContact = new ContactModel({
+    name: request.body.name,
+    occupation: request.body.occupation,
+    avatar: request.body.avatar
+  });
+
+  newContact.save()
+    .then(contact => {
+      return response.json(contact);
+    })
+    .catch(err => {
+      return console.log(`Error ${err}`);
+    });
+});
+
+app.delete('/addedContacts/:_id', function (request, response) {
+  ContactModel.findByIdAndRemove(request.params._id).exec()
+    .then(contact => {
+      return response.json(contact);
+    })
+    .catch(err => {
+      return console.log(`Error ${err}`);
+    });
 });
 
 
--- src/models/ContactModel.js
+++ src/models/ContactModel.js
@@ -18,4 +18,4 @@ const contactSchema = new mongoose.Schema({
 
 });
 
-module.exports = mongoose.model('Contact', contactSchema);
+export default mongoose.model('Contact', contactSchema);
