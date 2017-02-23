d26b7731db27831c65571b44fe2b2524564f6f19
Bradley Cuccio <bradleycuccio@gmail.com>
2017-02-23 10:14:08 -0600

    Completed Contact API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-// import the express module
+// Require the express module 
 import express from 'express';
 import bodyParser from 'body-parser';
 
-// create a new instance of express
+// Create a new instance of express
 const app = express();
 
 // Connect to our mongo database
@@ -12,98 +12,19 @@ mongoose.connect('mongodb://localhost/contact-list');
 
 app.use(bodyParser.json());
 
-import ContactModel from './models/ContactModel';
+import contactRoutes from './routes/ContactRoutes';
+app.use(contactRoutes);
 
-// declare our get /contacts route
-app.get('/contacts', (request, response) => {
-  ContactModel.find({})
-  .exec()
-  .then(contacts => {
-    return response.json(contacts);
-  })
-  .catch(err => {
-    return console.log(`Error ${err}`);
-  });
-});
-
-app.get('/contacts/:_id', (request, response) => {
-  ContactModel.findById(request.params._id)
-  .exec()
-  .then(contact => {
-    return response.json(contact);
-  })
-  .catch(err => {
-    return console.log(`Error ${err}`);
-  });
-});
-
-// delete
-app.delete('contacts/:_id', (request, response) => {
-  ContactModel.findByIdAndRemove(request.params._id)
-  .exec()
-  .then(contact => {
-    return response.json(contact);
-  })
-  .catch(err => {
-    return console.log(`Error ${err}`);
-  });
-});
-
-// create
-app.post('/contacts', (request, response) => {
-// Create a new instance of our `ContactModel`
-// We are grabbing attributes from our request.body object, again this is set
-// for us because we are using body-parser
-  const contact = new ContactModel({
-    name: request.body.name,
-    occupation: request.body.occupation,
-    avatar: request.body.avatar
-  });
-
-// Save the new contact
-  contact
-    .save()
-// when the save completes, return the newly created contact
-  .then(newContact => {
-    return response.json(newContact);
-  })
-  .catch(err => {
-    return console.log(`Error ${err}`);
-  });
-});
-
-// update contact
-app.put('/contacts/:_id', (request, response) => {
-  ContactModel.findById(request.params._id)
-  .exec()
-  .then(contact => {
-// Set the attributes on the model from the request.body OR
-// if we receive nothing, what the contact is already set to
-// this way if we send an update for just the `avatar` field, the name and occupation wont change
-    contact.name = request.body.name || contact.name;
-    contact.occupation = request.body.occupation || contact.occupation;
-    contact.avatar = request.body.avatar || contact.avatar;
-
-    return contact.save();
-  })
-  .then( contact => {
-    return response.json(contact);
-  })
-  .catch(err => {
-    return console.log(`Error ${err}`);
-  });
-});
-
-// declare our route
-app.all('/*', (request, response) => {
-  return response.send(request.params['0']);
+/* eslint no-unused-vars: 0 */
+app.use(function (err, request, response, next) {
+  return response.status(500).send('Uh oh something went wrong! ' + err);
 });
 
 // Set our port to server the application on
 const PORT = 3001;
 
 // Tell our instance of express to listen to request made on our port
-app.listen(PORT, (err) => {
+app.listen(PORT, function (err) {
   if (err) {
     return console.log('Error!', err);
   }
